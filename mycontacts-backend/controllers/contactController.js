94601e1a917a3bf604f7a8df7e06648bc45071db
habibafaisal <habibafaisal8@gmail.com>
2024-09-30 20:44:50 +0500

    middleware setup

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,16 +1,17 @@
+const asyncHandler = require("express-async-handler");
 // @desc Get all contacts
 // @routes GET /api/contacts
 // @access public
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ mess: "get all contacts" });
-};
+});
 
 // @desc create contacts
 // @routes POST /api/contacts
 // @access public
 
-const postContact = (req, res) => {
+const postContact = asyncHandler(async (req, res) => {
   console.log("req body is", req.body);
   const { name, phone, email } = req.body;
   if (!name || !phone || !email) {
@@ -18,31 +19,31 @@ const postContact = (req, res) => {
     throw new Error("all fields are required");
   }
   res.status(201).json({ mess: "create contacts" });
-};
+});
 
 // @desc Get  contacts by id
 // @routes GET /api/contacts
 // @access public
 
-const getContactById = (req, res) => {
+const getContactById = asyncHandler(async (req, res) => {
   res.status(200).json({ mess: `get contact by id ${req.params.id}` });
-};
+});
 
 // @desc update  contacts by id
 // @routes PUT /api/contacts
 // @access public
 
-const updateContactById = (req, res) => {
+const updateContactById = asyncHandler((req, res) => {
   res.status(200).json({ mess: `update contact by id ${req.params.id}` });
-};
+});
 
 // @desc delete  contacts by id
 // @routes DELETE /api/contacts
 // @access public
 
-const deleteContactById = (req, res) => {
+const deleteContactById = asyncHandler((req, res) => {
   res.status(200).json({ mess: `delete contact by id ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContact,
